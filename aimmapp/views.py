abb6fbd3d9fbf835abce2810284341b44a438b5c
rjsausa <rjsausa@gmail.com>
2024-01-23 17:24:14 -0800

    urls and views connected

--- aimmapp/views.py
+++ aimmapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def project_list(req):
+    return render(req, 'aimm/project-list.html')
+
+def project_detail(req, project_slug):
+    return render(req, 'aimm/project-detail.html')
+
